e576c25db29dbaa1269361ad919e29eb9b19fa95
Gerardo <1911875k@umich.mx>
2025-11-10 18:59:24 -0600

    Creacion de tablas: student, class y advisor

--- backend/schema.sql
+++ backend/schema.sql
@@ -5,8 +5,46 @@ CREATE DATABASE IF NOT EXISTS newDB;
 USE newDB;
 
 -- crear la tabla  EXAMPLE
-CREATE TABLE IF NOT EXISTS codeLead (
+/*CREATE TABLE IF NOT EXISTS codeLead (
     id INT AUTO_INCREMENT PRIMARY KEY,
     code VARCHAR(255) NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);*/
+ 
+CREATE TABLE IF NOT EXISTS student (
+    registrationNumber VARCHAR(8) PRIMARY KEY NOT NULL, --Matricula, hace a la vez de id
+    firstName VARCHAR(64) NOT NULL,
+    lastName VARCHAR(64) NOT NULL,
+    birthDate DATE,
+    email VARCHAR(64) UNIQUE NOT NULL,
+    degree INT NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS advisor(
+    idAdvisor INT AUTO_INCREMENT PRIMARY KEY,
+    registrationNumber VARCHAR(8) UNIQUE KEY NOT NULL, --Como el asesor es estudiante tambien tiene una matricula
+    score NUMERIC(3,2), --Promedio del asesor
+    startDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --Fecha en que el alumno comenzo a dar asesorias
+    FOREIGN KEY (registrationNumber) REFERENCES student(registrationNumber)
+);
+
+
+CREATE TABLE IF NOT EXISTS class (
+    idClass INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    classroom VARCHAR(16), --Donde se imparte la clase
+    schedule DATETIME, --Fecha y hora de la clase
+    idAdvisor INT NOT NULL, --ID del asesor de la clase
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (idAdvisor) REFERENCES advisor(idAdvisor)
+);
+
+-- Tabla intermedia para la relación muchos-a-muchos entre class y student
+CREATE TABLE IF NOT EXISTS class_student (
+    idClass INT NOT NULL,
+    registrationNumber VARCHAR(8) NOT NULL, --Estudiantes que asisten a la asesoria
+    PRIMARY KEY (idClass, registrationNumber), --Llave primaria compuesta
+    FOREIGN KEY (idClass) REFERENCES class(idClass),
+    FOREIGN KEY (registrationNumber) REFERENCES student(registrationNumber)
 );

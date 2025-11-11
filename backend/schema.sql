c21e038c44831a51aa37409635775a34315a45ca
Alejandra Oropeza <2201144c@umich.mx>
2025-11-10 21:49:35 -0600

    Adds database schema definition

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,12 +1,37 @@
 -- crear la base de datos
-CREATE DATABASE IF NOT EXISTS newDB;
+CREATE DATABASE IF NOT EXISTS asesorias;
 
 -- usar la base de datos
-USE newDB;
+USE asesorias;
 
--- crear la tabla  EXAMPLE
-CREATE TABLE IF NOT EXISTS codeLead (
+-- tabla student
+CREATE TABLE IF NOT EXISTS student (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    code VARCHAR(255) NOT NULL,
+    name VARCHAR(64) NOT NULL,
+    matricula VARCHAR(16) NOT NULL, -- matricula del estudiante
+    dob DATE NOT NULL, -- fecha de nacimiento
+    email VARCHAR(64) NOT NULL,
+    phone VARCHAR(16) NOT NULL,
+    degree VARCHAR(32) NOT NULL, -- carrera
+    grade VARCHAR(8) NOT NULL, -- promedio general
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- tabla advisor
+CREATE TABLE IF NOT EXISTS advisor (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_student INT NOT NULL UNIQUE, -- su id en la tabla student
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_student) REFERENCES student(id)
+);
+
+-- tabla class
+CREATE TABLE IF NOT EXISTS class (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(64) NOT NULL, -- nombre de la clase
+    id_advisor INT NOT NULL, -- asesor asignado
+    scheduleinfo VARCHAR(64) NOT NULL, -- información del horario
+    location VARCHAR(32) NOT NULL, -- lugar de la clase
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_advisor) REFERENCES advisor(id)
 );

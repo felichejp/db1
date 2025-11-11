dbba228b5bd3e1746ccf8bb3cd29880267336d0a
victor ramon guzman barajas <victorhache04@gmail.com>
2025-11-11 06:05:25 -0600

    lo nuevo

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,12 +1,36 @@
 -- crear la base de datos
-CREATE DATABASE IF NOT EXISTS newDB;
+CREATE DATABASE IF NOT EXISTS asesorias;
 
 -- usar la base de datos
-USE newDB;
+USE asesorias;
 
 -- crear la tabla  EXAMPLE
-CREATE TABLE IF NOT EXISTS codeLead (
+CREATE TABLE IF NOT EXISTS student (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    code VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    matricula VARCHAR(255) NOT NULL,
+    career VARCHAR(255) NOT NULL,
+    semester INT NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Relaciona al asesor con un estudiante existente
+CREATE TABLE IF NOT EXISTS advisor (
+    id SERIAL PRIMARY KEY,
+    id_student INT NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_student) REFERENCES student(id) 
+);
+
+-- Tabla class
+CREATE TABLE IF NOT EXISTS class (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    id_advisor INT NOT NULL,
+    schedule_info VARCHAR(64) NOT NULL, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_advisor) REFERENCES advisor(id) 
 );

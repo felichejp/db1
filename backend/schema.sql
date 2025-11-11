b8a0504979955c482340c7ebed4a9fe4c6f316d3
Foxavant18 <huetamo12100@hotmail.com>
2025-11-10 22:22:15 -0600

    actualizado

--- backend/schema.sql
+++ backend/schema.sql
@@ -10,3 +10,24 @@ CREATE TABLE IF NOT EXISTS codeLead (
     code VARCHAR(255) NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS student (
+    student_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE -- 'UNIQUE' asegura que no haya emails repetidos
+);
+
+-- crear la tabla advisor
+CREATE TABLE IF NOT EXISTS advisor (
+    advisor_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE,
+    specialty TEXT -- La especialidad del asesor 
+);
+
+
+CREATE TABLE IF NOT EXISTS course (
+    class_id SERIAL PRIMARY KEY,
+    subject_name VARCHAR(255) NOT NULL, -- Ej. 'Programación 1', 'Álgebra Lineal'
+    description TEXT
+);

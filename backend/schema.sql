19ae3eed10de73632156009e629939ad3e5f8e38
Eduardo <1912353x@umich.mx>
2025-11-10 22:13:59 -0600

    Agregado tabla student, advisor y class al schema.sql

--- backend/schema.sql
+++ backend/schema.sql
@@ -4,9 +4,36 @@ CREATE DATABASE IF NOT EXISTS newDB;
 -- usar la base de datos
 USE newDB;
 
--- crear la tabla  EXAMPLE
-CREATE TABLE IF NOT EXISTS codeLead (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    code VARCHAR(255) NOT NULL,
-    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- Tabla student
+CREATE TABLE IF NOT EXISTS student (
+    idStudent SERIAL PRIMARY KEY,
+    nameStudent VARCHAR(64) NOT NULL,
+    lastNameStudent VARCHAR(64) NOT NULL,
+    institution VARCHAR(64) NOT NULL,
+    career VARCHAR(64) NOT NULL,
+    age INT NOT NULL,
+    phoneNumber VARCHAR(16) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    idAdvisor INTEGER REFERENCES advisor(idAdvisor),
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+-- Tabla advisor
+CREATE TABLE IF NOT EXISTS advisor (
+    idAdvisor SERIAL PRIMARY KEY,
+    nameAdvisor VARCHAR(64) NOT NULL,
+    lastNameAdvisor VARCHAR(64) NOT NULL,
+    institution VARCHAR(64) NOT NULL,
+    phoneNumber VARCHAR(16) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+--Tabla class
+CREATE TABLE IF NOT EXISTS class (
+    idClass SERIAL PRIMARY KEY,
+    nameClass VARCHAR(64) NOT NULL,
+    descriptionClass VARCHAR(255) NOT NULL,
+    idAdvisor INTEGER REFERENCES advisor(idAdvisor),
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );

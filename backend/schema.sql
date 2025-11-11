a690bbd20a150cccf38e527294ffc587d8e0c034
Sonia C <2201160a@umich.mx>
2025-11-11 06:07:14 -0600

    Se agrega tablas student, advisor y class al squema

--- backend/schema.sql
+++ backend/schema.sql
@@ -10,3 +10,40 @@ CREATE TABLE IF NOT EXISTS codeLead (
     code VARCHAR(255) NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+-- tabla student
+CREATE TABLE IF NOT EXISTS student (
+    id INT AUTO_INCREMENT PRIMARY KEY,// un id incremental para mi es mas fscil que se quede como la llave primaria en comparacion a la matricula, sin embargo, creo que igualmente se debe almacenar la matricula para cuestiones escolares
+    matricula VARCHAR(16) NOT NULL, //  
+    nombre VARCHAR(64) NOT NULL,
+    primerApellido VARCHAR(64) NOT NULL,
+    segundoApellido VARCHAR(64),
+    email VARCHAR(128) UNIQUE,
+    telefono VARCHAR(16), //telefono de contacto
+    whatsapp VARCHAR(16), //telefono con acceso a whatsapp
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--tabla advisor
+CREATE TABLE IF NOT EXISTS advisor (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    matricula VARCHAR(16) NOT NULL,
+    nombre VARCHAR(64) NOT NULL,
+    primerApellido VARCHAR(64) NOT NULL,
+    segundoApellido VARCHAR(64),
+    carrera VARCHAR(64),
+    email VARCHAR(128) UNIQUE,
+    telefono VARCHAR(16), //telefono de contacto
+    whatsapp VARCHAR(16), //telefono con acceso a whatsapp
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--tabla class
+CREATE TABLE IF NOT EXISTS class (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(64) NOT NULL,
+    horario VARCHAR(64), //si ya se va a almacenar en DB el horario ya no deberia cambiar
+    idAsesor INT NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (idAsesor) REFERENCES advisor(id)
+);
